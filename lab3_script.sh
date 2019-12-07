14055d4b87772b64b916dc8f5239bc585b99dfff
fpmull <fpmull@LAPTOP-1OKMMC7Q.localdomain>
2019-12-06 17:24:28 -0700

    dfdf

---
+++ lab3_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author : Ford Mulligan
+# Date : 9/18/2019
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+
+echo "Enter file name: "
+read file
+echo "Enter regular expression: "
+read regexpr
+grep -c  $regexpr $file
